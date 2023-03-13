79ee89921964e057c34fe3d17a1a73246be4cb56
JLuis Avalos <luizavalos40@gmail.com>
2023-03-13 15:35:33 -0500

    clean up

--- openai.js
+++ openai.js
@@ -1,6 +1,5 @@
 import { Configuration, OpenAIApi } from "openai";
 import { config } from "dotenv";
-import { createImage } from "./chatgpt.js";
 config();
 
 const apiKey = process.env.OPENAI_API_KEY;
@@ -9,5 +8,3 @@ const configuration = new Configuration({
 });
 
 export const openai = new OpenAIApi(configuration);
-
-// createImage("Cat with 3 eyes");
